90efbd9a551ca3cc1baa558efcd35de06df93beb
ThogaruchestiHemanth <saihemanth225@gmail.com>
2026-01-16 11:39:51 +0530

    function called added

--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import com.example.composeplayground.ui.BusinessCard
 import com.example.composeplayground.ui.theme.ComposeTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,7 +15,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTestTheme {
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    Text("hello")
+                    BusinessCard()
                 }
             }
         }

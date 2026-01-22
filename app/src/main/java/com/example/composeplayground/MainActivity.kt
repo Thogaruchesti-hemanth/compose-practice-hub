330f52b35a0625bc68e66939e36cf723119de81a
ThogaruchestiHemanth <saihemanth225@gmail.com>
2026-01-22 17:24:42 +0530

    Dice Roller App initial commit

--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -3,9 +3,14 @@ package com.example.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.example.composeplayground.ui.theme.ComposeTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,10 +18,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTestTheme {
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    Text("hello")
-                }
+                DiceRollerApp()
             }
         }
     }
+}
+
+
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    )
 }
